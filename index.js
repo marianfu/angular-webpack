31ea8219aa7ed804a0a174bf606aedc016ba909b
mariano.furriel <mariano.furriel@fdvsolutions.com>
2016-08-05 17:21:31 -0300

    parser added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var jquery = require('jquery');
 var angular = require('./js/lib/angular');
+var Parser = require('./js/parser');
+//var Escodegen = require('escodegen');
 
 var myApp = angular.module('myApp', ['ui.ace']);
 myApp.controller('mainCtrl', require('./js/controllers/mainCtrl'));
 myApp.controller("controller", ["$scope", function($scope) {
-		$scope.code = "alert('hello world');";
+		$scope.code = "";
+		$scope.code2 = "";
+
+		$scope.change = function() {
+			var str = Parser.parse($scope.code);
+			if(str != null)
+				$scope.code2 = JSON.stringify(str);
+		};
 }]);
